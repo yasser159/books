fb05906bcd26dc0e1a2501395aea55126f421da6
Yasser Hajlaoui <yasser159@gmail.com>
2023-12-04 23:30:16 -0800

    Delete unnecessary test files and unused code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    // BAD CODE!
+    books.push({ id: 123, title: title });
+    console.log(books);
+    setBooks(books);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.length}
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
